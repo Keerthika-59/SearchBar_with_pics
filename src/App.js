081498e6b121b7438a2681cefcf96b7ebed80962
Keerthika-59 <82517053+Keerthika-59@users.noreply.github.com>
2022-05-15 21:28:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Searchbar from "./components/SearchBar";
+export default function App() {
+  return (
+    <div className="App">
+      <Searchbar />
+    </div>
+  );
+}
---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Enter Input:</label>
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
